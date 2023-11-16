46cbbae75b4eb3afc94e15ed603391d9f62489e4
egroehs <eduardagroehs@gmail.com>
2023-11-16 14:52:43 -0800

    Inject the forecast HTML from JS

--- safe.js
+++ safe.js
@@ -64,34 +64,3 @@ let currentDate = new Date();
 currentDateElement.innerHTML = formatDate(currentDate);
 
 searchCity("Paris");
-
-//Forecast
-
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHtml = "";
-
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-      <div class="weather-forecast-day">
-
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>15º</strong>
-          </div>
-          <div class="weather-forecast-temperature">9º</div>
-          
-        </div>
-      </div>
-    `;
-  });
-
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
-
-displayForecast();
